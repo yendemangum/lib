b3dd123ba61fcea7c81bcc68a49d8a10345a8c77
Yende Mangum <yendemangum@gmail.com>
2024-10-10 16:35:15 -0400

    Create Library
    Library allows user to add and delete books. User can toggle between
    read/unread states for books in library.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # lib
+
+This project involves creating a library that allows the user to add and remove books. The user can toggle between read and unread states for each book. 
---
+++ script.js
@@ -0,0 +1,72 @@
+let myLibrary = [];
+
+function Book(title, author, pageNum, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pageNum = pageNum;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    bookForm.style.display = "none";
+}
+
+const bookForm = document.querySelector(".book-form");
+
+bookForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(bookForm);
+    
+    const book = new Book(formData.get("title"), formData.get("author"), formData.get("page-num"), formData.get("is-read"));
+    addBookToLibrary(book);
+    showBooks();
+    newBook.style.display = 'inline-block'
+    newBook.style.alignSelf = 'center'
+})
+
+const newBook = document.querySelector(".new-book")
+
+newBook.addEventListener("click", () => {
+    bookForm.style.display = "flex"
+    bookForm.style.flexDirection = "column"
+    bookForm.style.alignItems = "center"
+    bookForm.style.gap = ".5rem"
+    bookForm.style.padding = ".5rem"
+    newBook.style.display = "none"
+})
+
+function showBooks() {
+    const books = document.querySelector(".books")
+    books.textContent = ""
+    for (let i = 0; i < myLibrary.length; i++) {
+        const book = document.createElement("div")
+        book.setAttribute("class", "each-book")
+        const button = document.createElement("button")
+        const button2 = document.createElement("button")
+        button2.textContent = "Mark as read/unread"
+        button.textContent = "Delete"
+        const thisBook = myLibrary[i];
+        book.textContent = thisBook.title + " by " + thisBook.author + ", " + thisBook.pageNum + " pages, " + thisBook.isRead;
+        books.appendChild(book)        
+        book.appendChild(button)
+        book.appendChild(button2)
+        button2.addEventListener("click", () =>{
+            if (thisBook.isRead == "read") {
+                thisBook.isRead = "unread"
+            }
+            else if (thisBook.isRead == "unread") {
+                thisBook.isRead = "read"
+            }
+            showBooks();
+        })
+        button.addEventListener("click", () => {
+            myLibrary = myLibrary.splice(i,i)
+            books.removeChild(book)
+            book.removeChild(button)
+            book.removeChild(button2)
+        })
+    }
+    
+}
